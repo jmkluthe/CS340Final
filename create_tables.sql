6ca3d0beec6a05daa815c9a21118375db4151c12
jmkluthe <kluthej@oregonstate.edu>
2016-11-25 15:44:57 -0800

    add schema, update ER diagram, update sql create and insert queries
    add a schema and edit er diagram to more accurately reflect the
    database.  change some field names and constraints in sql scripts.

--- create_tables.sql
+++ create_tables.sql
@@ -1,54 +1,54 @@
 #-- Table Creation Script
 
 #-- Drop tables before recreating
-DROP TABLE IF EXISTS person;
-DROP TABLE IF EXISTS movie;
-DROP TABLE IF EXISTS company;
-DROP TABLE IF EXISTS genre;
-DROP TABLE IF EXISTS award;
-DROP TABLE IF EXISTS movie_to_person;
-DROP TABLE IF EXISTS movie_to_award;
+SET FOREIGN_KEY_CHECKS = 0;
+DROP TABLE IF EXISTS Person;
+DROP TABLE IF EXISTS Company;
+DROP TABLE IF EXISTS Genre;
+DROP TABLE IF EXISTS Award;
+DROP TABLE IF EXISTS Movie;
+DROP TABLE IF EXISTS Movie_to_Person;
+DROP TABLE IF EXISTS Movie_to_Award;
+SET FOREIGN_KEY_CHECKS = 1;
 
 #-- nothing special, a person has a first, middle, last name and date of birth
-CREATE TABLE person (
+CREATE TABLE Person (
 	id INT NOT NULL AUTO_INCREMENT,
-	f_name VARCHAR(255) NOT NULL,
-	m_name VARCHAR(255),
-	l_name VARCHAR(255) NOT NULL,
+	first_name VARCHAR(255) NOT NULL,
+	last_name VARCHAR(255) NOT NULL,
 	dob DATE,
 	PRIMARY KEY (id)
 ) ENGINE=InnoDB;
 
 #-- a company has a name
-CREATE TABLE company (
+CREATE TABLE Company (
 	id INT NOT NULL AUTO_INCREMENT,
 	name VARCHAR(255) NOT NULL,
 	PRIMARY KEY (id)
 ) ENGINE=InnoDB;
 
 #-- a genre has a name
-CREATE TABLE genre (
+CREATE TABLE Genre (
 	id INT NOT NULL AUTO_INCREMENT,
 	name VARCHAR(255) NOT NULL,
 	PRIMARY KEY (id)
 ) ENGINE=InnoDB;
 
 #-- a movie has a title and release date, and a reference to a company and a genre
-CREATE TABLE movie (
+CREATE TABLE Movie (
 	id INT NOT NULL AUTO_INCREMENT,
 	title VARCHAR(255) NOT NULL,
-	release_date DATE,
-	company_id INT,
-	genre_id INT,
+	company_id INT NOT NULL,
+	genre_id INT NOT NULL,
 	PRIMARY KEY (id),
-	FOREIGN KEY (company_id) REFERENCES company (id)
-	ON DELETE SET NULL ON UPDATE SET NULL,
-	FOREIGN KEY (genre_id) REFERENCES genre (id)
-	ON DELETE SET NULL ON UPDATE SET NULL
+	FOREIGN KEY (company_id) REFERENCES Company (id)
+	ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (genre_id) REFERENCES Genre (id)
+	ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB;
 
 
-CREATE TABLE award (
+CREATE TABLE Award (
 	id INT NOT NULL AUTO_INCREMENT,
 	name VARCHAR(255) NOT NULL,
 	PRIMARY KEY (id)
@@ -56,25 +56,25 @@ CREATE TABLE award (
 
 
 #-- job_role is part of the primary key to allow the same person to, for instance, both act and direct in a movie
-CREATE TABLE movie_to_person (
+CREATE TABLE Movie_to_Person (
 	movie_id INT NOT NULL,
 	person_id INT NOT NULL,
 	job_role VARCHAR(255) NOT NULL,
 	PRIMARY KEY (movie_id, person_id, job_role),
-	FOREIGN KEY (movie_id) REFERENCES movie (id)
+	FOREIGN KEY (movie_id) REFERENCES Movie (id)
 	ON DELETE CASCADE ON UPDATE CASCADE,
-	FOREIGN KEY (person_id) REFERENCES person (id)
+	FOREIGN KEY (person_id) REFERENCES Person (id)
 	ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB;
 
 
-CREATE TABLE movie_to_award (
+CREATE TABLE Movie_to_Award (
 	movie_id INT NOT NULL,
 	award_id INT NOT NULL,
 	PRIMARY KEY (movie_id, award_id),
-	FOREIGN KEY (movie_id) REFERENCES movie (id)
+	FOREIGN KEY (movie_id) REFERENCES Movie (id)
 	ON DELETE CASCADE ON UPDATE CASCADE,
-	FOREIGN KEY (award_id) REFERENCES award (id)
+	FOREIGN KEY (award_id) REFERENCES Award (id)
 	ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB;
 
--- insert_values.sql
+++ insert_values.sql
@@ -5,62 +5,62 @@
 #-- delete all the table data before re-adding it
 #-- must turn off foreign key checking to allow truncation
 SET FOREIGN_KEY_CHECKS = 0;
-TRUNCATE TABLE movie_to_person;
-TRUNCATE TABLE movie_to_award;
-TRUNCATE TABLE person;
-TRUNCATE TABLE movie;
-TRUNCATE TABLE company;
-TRUNCATE TABLE genre;
-TRUNCATE TABLE award;
+TRUNCATE TABLE Movie_to_Person;
+TRUNCATE TABLE Movie_to_Award;
+TRUNCATE TABLE Person;
+TRUNCATE TABLE Movie;
+TRUNCATE TABLE Company;
+TRUNCATE TABLE Genre;
+TRUNCATE TABLE Award;
 SET FOREIGN_KEY_CHECKS = 1;
 
-INSERT INTO person VALUES 
-(1,'PENELOPE','MIDDLE','GUINESS','2006-02-15 04:34:33'),
-(2,'NICK','MIDDLE','WAHLBERG','2006-02-15 04:34:33'),
-(3,'ED','MIDDLE','CHASE','2006-02-15 04:34:33'),
-(4,'JENNIFER','MIDDLE','DAVIS','2006-02-15 04:34:33');
+INSERT INTO Person VALUES 
+(1,'PENELOPE','GUINESS','2006-02-15 04:34:33'),
+(2,'NICK','WAHLBERG','2006-02-15 04:34:33'),
+(3,'ED','CHASE','2006-02-15 04:34:33'),
+(4,'JENNIFER','DAVIS','2006-02-15 04:34:33');
 
-INSERT INTO movie VALUES
-(1, 'Jurassic Park', '1993-06-06 00:00:00', NULL, NULL),
-(2, 'V for Vendetta', '2006-06-06 00:00:00', NULL, NULL),
-(3, 'Titanic', '1997-06-06 00:00:00', NULL, NULL),
-(4, 'Independence Day', '1996-06-06 00:00:00', NULL, NULL);
-
-INSERT INTO company VALUES
+INSERT INTO Company VALUES
 (1, 'Warner Bros'),
 (2, 'Disney'),
 (3, '20th Century Fox');
 
-INSERT INTO award VALUES
+INSERT INTO Award VALUES
 (1, 'Best Picture'),
 (2, 'Best Effects'),
 (3, 'Best Stuff');
 
-INSERT INTO genre VALUES
+INSERT INTO Genre VALUES
 (1, 'Action'),
 (2, 'Aliens'),
 (3, 'Ships/Romance');
 
-INSERT INTO movie_to_person VALUES
-((SELECT id FROM movie WHERE title = 'Jurassic Park'),(SELECT id FROM person WHERE f_name = 'PENELOPE' AND l_name = 'GUINESS'),'Actor'),
-((SELECT id FROM movie WHERE title = 'Jurassic Park'),(SELECT id FROM person WHERE f_name = 'PENELOPE' AND l_name = 'GUINESS'),'Director'),
-((SELECT id FROM movie WHERE title = 'V for Vendetta'),(SELECT id FROM person WHERE f_name = 'NICK' AND l_name = 'WAHLBERG'),'Actor'),
-((SELECT id FROM movie WHERE title = 'Titanic'),(SELECT id FROM person WHERE f_name = 'ED' AND l_name = 'CHASE'),'Director'),
-((SELECT id FROM movie WHERE title = 'Independence Day'),(SELECT id FROM person WHERE f_name = 'JENNIFER' AND l_name = 'DAVIS'),'Actor');
+INSERT INTO Movie VALUES
+(1, 'Jurassic Park', (SELECT c.id FROM Company c WHERE c.name = "Warner Bros"), (SELECT g.id FROM Genre g WHERE g.name = "Action")),
+(2, 'V for Vendetta', (SELECT c.id FROM Company c WHERE c.name = "20th Century Fox"), (SELECT g.id FROM Genre g WHERE g.name = "Action")),
+(3, 'Titanic', (SELECT c.id FROM Company c WHERE c.name = "Disney"), (SELECT g.id FROM Genre g WHERE g.name = "Ships/Romance")),
+(4, 'Independence Day', (SELECT c.id FROM Company c WHERE c.name = "Warner Bros"), (SELECT g.id FROM Genre g WHERE g.name = "Aliens"));
 
-INSERT INTO movie_to_award VALUES
-((SELECT id FROM movie WHERE title = 'Jurassic Park'),(SELECT id FROM award WHERE name = 'Best Picture')),
-((SELECT id FROM movie WHERE title = 'Jurassic Park'),(SELECT id FROM award WHERE name = 'Best Effects')),
-((SELECT id FROM movie WHERE title = 'V for Vendetta'),(SELECT id FROM award WHERE name = 'Best Stuff')),
-((SELECT id FROM movie WHERE title = 'Titanic'),(SELECT id FROM award WHERE name = 'Best Picture')),
-((SELECT id FROM movie WHERE title = 'Independence Day'),(SELECT id FROM award WHERE name = 'Best Effects'));
+INSERT INTO Movie_to_Person VALUES
+((SELECT id FROM Movie WHERE title = 'Jurassic Park'),(SELECT id FROM Person WHERE first_name = 'PENELOPE' AND last_name = 'GUINESS'),'Actor'),
+((SELECT id FROM Movie WHERE title = 'Jurassic Park'),(SELECT id FROM Person WHERE first_name = 'PENELOPE' AND last_name = 'GUINESS'),'Director'),
+((SELECT id FROM Movie WHERE title = 'V for Vendetta'),(SELECT id FROM Person WHERE first_name = 'NICK' AND last_name = 'WAHLBERG'),'Actor'),
+((SELECT id FROM Movie WHERE title = 'Titanic'),(SELECT id FROM Person WHERE first_name = 'ED' AND last_name = 'CHASE'),'Director'),
+((SELECT id FROM Movie WHERE title = 'Independence Day'),(SELECT id FROM Person WHERE first_name = 'JENNIFER' AND last_name = 'DAVIS'),'Actor');
 
-UPDATE movie SET company_id = (SELECT c.id FROM company c WHERE c.name = "Warner Bros") WHERE title = 'Jurassic Park';
-UPDATE movie SET company_id = (SELECT c.id FROM company c WHERE c.name = "20th Century Fox") WHERE title = 'V for Vendetta';
-UPDATE movie SET company_id = (SELECT c.id FROM company c WHERE c.name = "Disney") WHERE title = 'Titanic';
-UPDATE movie SET company_id = (SELECT c.id FROM company c WHERE c.name = "Warner Bros") WHERE title = 'Independence Day';
+INSERT INTO Movie_to_Award VALUES
+((SELECT id FROM Movie WHERE title = 'Jurassic Park'),(SELECT id FROM Award WHERE name = 'Best Picture')),
+((SELECT id FROM Movie WHERE title = 'Jurassic Park'),(SELECT id FROM Award WHERE name = 'Best Effects')),
+((SELECT id FROM Movie WHERE title = 'V for Vendetta'),(SELECT id FROM Award WHERE name = 'Best Stuff')),
+((SELECT id FROM Movie WHERE title = 'Titanic'),(SELECT id FROM Award WHERE name = 'Best Picture')),
+((SELECT id FROM Movie WHERE title = 'Independence Day'),(SELECT id FROM Award WHERE name = 'Best Effects'));
 
-UPDATE movie SET genre_id = (SELECT g.id FROM genre g WHERE g.name = "Action") WHERE title = 'Jurassic Park';
-UPDATE movie SET genre_id = (SELECT g.id FROM genre g WHERE g.name = "Action") WHERE title = 'V for Vendetta';
-UPDATE movie SET genre_id = (SELECT g.id FROM genre g WHERE g.name = "Ships/Romance") WHERE title = 'Titanic';
-UPDATE movie SET genre_id = (SELECT g.id FROM genre g WHERE g.name = "Aliens") WHERE title = 'Independence Day';
+#--UPDATE Movie SET company_id = (SELECT c.id FROM company c WHERE c.name = "Warner Bros") WHERE title = 'Jurassic Park';
+#--UPDATE Movie SET company_id = (SELECT c.id FROM company c WHERE c.name = "20th Century Fox") WHERE title = 'V for Vendetta';
+#--UPDATE Movie SET company_id = (SELECT c.id FROM company c WHERE c.name = "Disney") WHERE title = 'Titanic';
+#--UPDATE Movie SET company_id = (SELECT c.id FROM company c WHERE c.name = "Warner Bros") WHERE title = 'Independence Day';
+
+#--UPDATE Movie SET genre_id = (SELECT g.id FROM Genre g WHERE g.name = "Action") WHERE title = 'Jurassic Park';
+#--UPDATE Movie SET genre_id = (SELECT g.id FROM Genre g WHERE g.name = "Action") WHERE title = 'V for Vendetta';
+#--UPDATE Movie SET genre_id = (SELECT g.id FROM Genre g WHERE g.name = "Ships/Romance") WHERE title = 'Titanic';
+#--UPDATE Movie SET genre_id = (SELECT g.id FROM Genre g WHERE g.name = "Aliens") WHERE title = 'Independence Day';
